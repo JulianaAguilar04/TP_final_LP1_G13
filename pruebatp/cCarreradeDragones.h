62c4ac22aafeaaa9c14fcc34aba43807377dd9ab
milagrosmenendeztuja <143182953+milagrosmenendeztuja@users.noreply.github.com>
2024-07-16 11:16:27 -0300

    limpieza je

--- pruebatp/cCarreradeDragones.h
+++ pruebatp/cCarreradeDragones.h
@@ -44,28 +44,3 @@ public:
     void anunciarGanador();
     ~cCarreradeDragones();
 };
-
-
-/*
-
-LO COMENTO PARA HACERLO DE NUEVO Y VER QUE FALLA (15/07 -15hs)
-#pragma once
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
-
-class cCarreradeDragones {
-public:
-    cCarreradeDragones(vector<cJinete*>& Jinetes_isla);
-    void iniciarCarrera();
-
-private:
-    vector<cJinete*>& jinetes;
-    void contarRegreso();
-};
-
-*/
-
